bc49cc3dda4d8ef4f576a626ad37070d35e18f4a
flyyoung708 <flyyoung708@gmail.com>
2017-02-11 07:44:27 -0500

    add variable size
    this release allows the user to specify the amount of data points to
    average over and the number of data points to be thrown out as outliers.
    it also cleans up some of the variables used.

--- WfilterDouble.cpp
+++ WfilterDouble.cpp
@@ -2,63 +2,87 @@
 #include "WfilterDouble.h"
 
 
-WfilterDouble::WfilterDouble(int check){
-_outliers = 3;
-_val = 0;
-_init = 0;
-_i = 0;
+WfilterDouble::WfilterDouble() {
+	numReadingsCollected = 20;
+	outliersRemoved = 4;
+	Setup(numReadingsCollected, outliersRemoved);
+}
+
+WfilterDouble::WfilterDouble(int numReadingsCollected) {
+	numReadingsCollected = numReadingsCollected;
+	if (numReadingsCollected < 3)
+		outliersRemoved = 1;
+	else
+		outliersRemoved = numReadingsCollected / 4;
+	Setup(numReadingsCollected, outliersRemoved);
+}
+
+WfilterDouble::WfilterDouble(int numReadingsCollected, int outliersRemoved) {
+	numReadingsCollected = numReadingsCollected;
+	outliersRemoved = outliersRemoved;
+	Setup(numReadingsCollected, outliersRemoved);
+}
+
+WfilterDouble::~WfilterDouble() {
+	delete[] _valArray;
+	delete[] _valArrayin;
 }
 
 
-double WfilterDouble::filtered(double reading){
-_reading = reading;
-if (_init == 0){
-  delay(1000);
-  for (int _w = 0; _w < 10; _w++){                // get a full set of measures at startup )
-    _valArrayin[_w] = 0;       // otherwise all vals ignored since deviation from zero
-  }
-  _init = 1;
-}
-  
-_valArrayin[_i] = _reading; 
+double WfilterDouble::filtered(double reading) {
+	_reading = reading;
 
+	_valArrayin[_i] = _reading;
 
-  for (int _k = 0; _k < 10; _k++){               
-    _valArray[_k] = _valArrayin[_k];
-}
+	for (int k = 0; k < arraysize; k++) {
+		_valArray[k] = _valArrayin[k];
+	}
 
-for (_z = 0; _z < _outliers; _z++){
-   _maxval = -1000000;
-   _minval = 1000000;
-  for (_j = 0; _j < 10; _j++){
-    if (_valArray[_j] >= _maxval){
-      _maxvalind = _j;
-      _maxval = _valArray[_j];
-    }
-    if (_valArray[_j] <= _minval){
-      _minvalind = _j;
-      _minval = _valArray[_j];
+	for (int k = 0; k < _outliers; k++) {
+		_maxval = -1000000;
+		_minval = 1000000;
+		_maxvalind = 0;
+		_minvalind = 0;
+		for (int j = 0; j < arraysize; j++) {
+			if (_valArray[j] >= _maxval) {
+				_maxvalind = j;
+				_maxval = _valArray[j];
+			}
+			if (_valArray[j] <= _minval) {
+				_minvalind = j;
+				_minval = _valArray[j];
 
-    }
-  } 
+			}
+		}
 
-  _valArray[_maxvalind] = _val;
-  _valArray[_minvalind] = _val;
- 
-}
+		_valArray[_maxvalind] = _val;
+		_valArray[_minvalind] = _val;
 
-_val = 0;
-for (int _r = 0; _r < 10; _r++){
-  _val =(_val + _valArray[_r])/2;
-  
-}
+	}
 
-_i++;
-if (_i > 9){
-  _i = 0;
-}
-return _val;
+	_sum = 0;
+	for (int r = 0; r < arraysize; r++) {
+		_sum += _valArray[r];
+	}
+
+	_val = _sum/divider;
+
+	_i++;
+	if (_i >= arraysize) {
+		_i = 0;
+	}
+
+	return _val;
 }
 
-
-
+void WfilterDouble::Setup(int numReadingsCollected, int outliersRemoved) {
+	_valArrayin = new double[numReadingsCollected];
+	_valArray = new double[numReadingsCollected];
+	//std::cout << _valArrayin << std::endl; 
+	//std::cout << _valArray << std::endl; 
+		arraysize = numReadingsCollected;
+		divider = arraysize;
+		_outliers = outliersRemoved;
+		_val = 0;
+		_i = 0;
+}
--- WfilterDouble.h
+++ WfilterDouble.h
@@ -3,30 +3,33 @@
 
 #include "Arduino.h"
 
-
 class WfilterDouble
-{ 
+{
 public:
-	WfilterDouble(int check);
+	WfilterDouble();
+	~WfilterDouble();
+	WfilterDouble(int numReadingsCollected);
+	WfilterDouble(int numReadingsCollected, int outliersRemoved);
 	double filtered(double reading);
+
 private:
+	int numReadingsCollected;
+	int outliersRemoved;
 	double _reading;
-	double _outliers;
 	double _val;
-	double _valArrayin[10];
-	double _valArray[10];
-	int _init;
+	double * _valArrayin;
+	double * _valArray;
 	double _maxval;
 	double _minval;
 	int _maxvalind;
 	int _minvalind;
 	unsigned long _sum;
 	int _i;
-	int _w;
-	int _k;
-	int _z;
-	int _j;
-	int _r;
+	int arraysize;
+	int _outliers;
+	double divider; 
+
+	void Setup(int numReadingsCollected, int outliersRemoved);
 };
 
 #endif
